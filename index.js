33c4a80aa76d3fea7092c0527312033b149e72d3
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-18 08:55:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// window.customerName = 'bob';
+var customerName='bob'
+//convert to uppercase
+function upperCaseCustomerName() {
+    // window.customerName=window.customerName.toUpperCase();
+    customerName=customerName.toUpperCase()
+    //window is used to access a global variable inside a function
+}
+// upperCaseCustomerName(customerName)
+
+function setBestCustomer(){
+   window.bestCustomer='not bob'
+    
+}
+function overwriteBestCustomer(){
+     window.bestCustomer='maybe bob'
+}
+//  overwritesBestcustomer()
+const leastFavoritecustomer='nyaga'
+function changeLeastFavoriteCustomer(){
+    leastFavoritecustomer='wawira'
+}
